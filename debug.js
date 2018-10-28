4cacd9627c4d8fbdca4a047ab9e0a12d2b998bf5
Yohanes Lumentut <yohanes.lumentut@yahoo.com>
2018-10-28 23:58:04 +0700

    fix broken package 0.0.6 - 0.0.7

--- debug.js
+++
@@ -1,6 +0,0 @@
-(function (window) {
-  if (!window.parent.initDone && window.location.pathname === '/context.html') {
-    window.parent.initDone = true;
-    window.open('/debug.html', '_blank');
-  }
-})(window)
