367491a20d4f65651d1277a45fbdc5c162ce1a34
Yohanes Oktavianus Lumentut <yohanes.lumentut@yahoo.com>
2019-05-23 14:49:46 +0700

    Update ng-ripple.module.ts

--- projects/ng-ripple-module/src/lib/ng-ripple.module.ts
+++ projects/ng-ripple-module/src/lib/ng-ripple.module.ts
@@ -11,28 +11,6 @@ import { RippleDirective } from './ripple.directive';
 import { CoreComponent } from './ripple-core.component';
 import { BackgroundComponent } from './ripple-bg.component';
 
-// import { Subject, Subscription } from 'rxjs';
-
-// const _windowDispatcher = new Subject();
-// const _windowResize = evt => _windowDispatcher.next(evt);
-
-// window.addEventListener("resize", _windowResize);
-
-// export function WindowSensor({
-//     event = "onWindowResize",
-//     destroyer = "ngOnDestroy"
-//   } = {}) {
-//   return function(constructor) {
-//     const subscription = new Subscription();
-//     subscription.add(_windowDispatcher.subscribe((evt) => constructor.prototype[event](evt)));
-//     const original = constructor.prototype[destroyer];
-//     constructor.prototype[destroyer] = function () {
-//       subscription.unsubscribe();
-//       original && typeof original === 'function' && original.apply(this, arguments);
-//     }
-//   }
-// }
-
 @NgModule({
   entryComponents: [
     CoreComponent,
